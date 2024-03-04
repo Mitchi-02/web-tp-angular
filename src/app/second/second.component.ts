455970d195626d18b6fbeb8dde2a46aa99b94a56
Ilyas Benhammadi <i.benhammadi@esi-sba.dz>
2024-03-04 08:22:41 +0100

    form

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,12 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-second',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './second.component.html',
-  styleUrl: './second.component.css'
+  styleUrl: './second.component.css',
 })
 export class SecondComponent {
-
+  f2 = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(10),
+    ]),
+    password: new FormControl('', [Validators.required]),
+    confirmation: new FormControl('', []),
+    country: new FormControl('', [Validators.required]),
+    sex: new FormControl('', [Validators.required]),
+    accept: new FormControl('', [Validators.required]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z]+@d.com$'),
+    ]),
+  });
+  get username() {
+    return this.f2.get('username');
+  }
+  get password() {
+    return this.f2.get('password');
+  }
+  get confirmation() {
+    return this.f2.get('confirmation');
+  }
+  get country() {
+    return this.f2.get('country');
+  }
+  get sex() {
+    return this.f2.get('sex');
+  }
+  get email() {
+    return this.f2.get('email');
+  }
+  get accept() {
+    return this.f2.get('accept');
+  }
 }
